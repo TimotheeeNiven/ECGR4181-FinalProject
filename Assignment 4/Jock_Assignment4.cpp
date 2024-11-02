96e757ef538a96bd10f9f914407e51220fce9536
Herodegon <ronaldjock45@gmail.com>
2024-11-02 13:02:55 -0400

    Merged assignment 3 with project

--- Assignment 4/Jock_Assignment4.cpp
+++ Assignment 4/Jock_Assignment4.cpp
@@ -1,10 +1,468 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 #include <map>
+#include <unordered_map>
+#include <variant>
+#include <cstdint>
+#include <bitset>
+#include <limits>
+
+//====ASSIGNMENT 3====
+
+// Define Funct7Map and InstructionMap
+using Funct7Map = std::unordered_map<int, std::string>;
+using InstructionMap = std::unordered_map<uint32_t, std::unordered_map<int, std::variant<std::string, Funct7Map>>>;
+
+// Define RISC-V opcodes
+#define OPCODE_LOAD         0b0000011
+#define OPCODE_LOAD_FP      0b0000111
+#define OPCODE_I_TYPE       0b0010011
+#define OPCODE_AUIPC        0b0010111
+#define OPCODE_S_TYPE       0b0100011
+#define OPCODE_S_TYPE_FP    0b0100111
+#define OPCODE_R_TYPE       0b0110011
+#define OPCODE_LUI          0b0110111
+#define OPCODE_SB_TYPE      0b1100011
+#define OPCODE_JALR         0b1100111
+#define OPCODE_JAL          0b1101111
+
+const int NO_IMMEDIATE = std::numeric_limits<int32_t>::max();
+const int NO_REGISTER = std::numeric_limits<int32_t>::max();;
+const int NO_FUNCT3 = std::numeric_limits<int32_t>::max();;
+const int NO_FUNCT7 = std::numeric_limits<int32_t>::max();;
+
+// Define instruction formats
+enum InstructionFormat {
+    FORMAT_R,
+    FORMAT_I,
+    FORMAT_S,
+    FORMAT_B,
+    FORMAT_U,
+    FORMAT_J
+};
+
+// Instruction map
+InstructionMap Instructions = 
+{
+    {
+        OPCODE_LOAD, 
+        {
+            {0b000, "lb"},
+            {0b001, "lh"},
+            {0b010, "lw"},
+            {0b100, "lbu"},
+            {0b101, "lhu"}
+        }
+    },
+    {
+        OPCODE_I_TYPE, 
+        {
+            {0b000, "addi"},
+            {0b001, "slli"},
+            {0b010, "slti"},
+            {0b011, "sltiu"},
+            {0b100, "xori"},
+            {0b101, Funct7Map{{0b0000000, "srli"}, {0b0100000, "srai"}}},
+            {0b110, "ori"},
+            {0b111, "andi"}
+        }
+    },
+    {
+        OPCODE_AUIPC, 
+        {
+            {NO_FUNCT3, "auipc"}
+        }
+    },
+    {
+        OPCODE_S_TYPE, 
+        {
+            {0b000, "sb"},
+            {0b001, "sh"},
+            {0b010, "sw"}
+        }
+    },
+    {
+        OPCODE_S_TYPE_FP, 
+        {
+            {0b010, "fsw"}
+        }
+    },
+    {
+        OPCODE_R_TYPE, 
+        {
+            {0b000, Funct7Map{{0b0000000, "add"}, {0b0100000, "sub"}, {0b0000001, "mul"}}},
+            {0b001, Funct7Map{{0b0000000, "sll"}, {0b0000001, "mulh"}}},
+            {0b010, Funct7Map{{0b0000000, "slt"}, {0b0000001, "mulhsu"}}},
+            {0b011, Funct7Map{{0b0000000, "sltu"}, {0b0000001, "mulhu"}}},
+            {0b100, Funct7Map{{0b0000000, "xor"}, {0b0000001, "div"}}},
+            {0b101, Funct7Map{{0b0000000, "srl"}, {0b0100000, "sra"}, {0b0000001, "divu"}}},
+            {0b110, Funct7Map{{0b0000000, "or"}, {0b0000001, "rem"}}},
+            {0b111, Funct7Map{{0b0000000, "and"}, {0b0000001, "remu"}}}
+        }
+    },
+    {
+        OPCODE_LUI, 
+        {
+            {NO_FUNCT3, "lui"}
+        }
+    },
+    {
+        OPCODE_SB_TYPE, 
+        {
+            {0b000, "beq"},
+            {0b001, "bne"},
+            {0b100, "blt"},
+            {0b101, "bge"},
+            {0b110, "bltu"},
+            {0b111, "bgeu"}
+        }
+    },
+    {
+        OPCODE_JALR, 
+        {
+            {0b000, "jalr"}
+        }
+    },
+    {
+        OPCODE_JAL,
+        {
+            {NO_FUNCT3, "jal"}
+        }
+    }
+};
+
+// Define control signals
+struct ControlSignals
+{
+    bool RegWrite = false;
+    bool MemRead = false;
+    bool MemWrite = false;
+    bool MemToReg = false;
+    bool ALUSrc = false;
+    bool Branch = false;
+    bool Jump = false;
+    bool JumpReg = false;
+    bool Zero = false;
+};
+
+// Instruction variables
+struct InstructionVariables
+{
+    int rs1 = NO_REGISTER;
+    int rs2 = NO_REGISTER;
+    int rd = NO_REGISTER;
+    int funct3 = NO_FUNCT3;
+    int funct7 = NO_FUNCT7;
+    int immediate = NO_IMMEDIATE;
+};
+
+// Control signals mapping
+std::unordered_map<uint8_t, ControlSignals> ControlInstructions = 
+{
+    {OPCODE_LOAD, {true, true, false, true, true, false, false, false, false}},
+    {OPCODE_LOAD_FP, {true, true, false, true, true, false, false, false, false}},
+    {OPCODE_I_TYPE, {true, false, false, false, true, false, false, false, false}},
+    {OPCODE_AUIPC, {true, false, false, false, true, false, false, false, false}},
+    {OPCODE_S_TYPE, {false, false, true, false, true, false, false, false, false}},
+    {OPCODE_S_TYPE_FP, {false, false, true, false, true, false, false, false, false}},
+    {OPCODE_R_TYPE, {true, false, false, false, false, false, false, false, false}},
+    {OPCODE_LUI, {true, false, false, false, true, false, false, false, false}},
+    {OPCODE_SB_TYPE, {false, false, false, false, false, true, false, false, false}},
+    {OPCODE_JALR, {true, false, false, false, true, false, true, true, false}},
+    {OPCODE_JAL, {true, false, false, false, true, false, true, false, false}}
+};
+
+class Simulator 
+{
+public:
+    Simulator() {};
+
+    // Decode instruction based on opcode
+    void decodeInstruction(uint32_t instruction)
+    {
+        uint8_t opcode = getOpcode(instruction);
+        ControlSignals signals = ControlInstructions[opcode];
+        InstructionVariables vars;
+        std::vector<std::string> printStatement;
+
+        InstructionFormat format;
+
+        switch(opcode)
+        {
+            case OPCODE_LOAD:
+            case OPCODE_LOAD_FP:
+            case OPCODE_I_TYPE:
+            case OPCODE_JALR:
+                format = FORMAT_I;
+                vars.rs1 = getRS1(instruction);
+                vars.rd = getRD(instruction);
+                vars.funct3 = getFunct3(instruction);
+                vars.immediate = getImmediate(instruction);
+                break;
+            case OPCODE_AUIPC:
+            case OPCODE_LUI:
+                format = FORMAT_U;
+                vars.rd = getRD(instruction);
+                vars.immediate = getImmediate(instruction);
+                break;
+            case OPCODE_S_TYPE:
+                format = FORMAT_S;
+                vars.rs1 = getRS1(instruction);
+                vars.rs2 = getRS2(instruction);
+                vars.funct3 = getFunct3(instruction);
+                vars.immediate = getImmediate(instruction);
+                break;
+            case OPCODE_SB_TYPE:
+                format = FORMAT_B;
+                vars.rs1 = getRS1(instruction);
+                vars.rs2 = getRS2(instruction);
+                vars.funct3 = getFunct3(instruction);
+                vars.immediate = getImmediate(instruction);
+                break;
+            case OPCODE_R_TYPE:
+                format = FORMAT_R;
+                vars.rs1 = getRS1(instruction);
+                vars.rs2 = getRS2(instruction);
+                vars.rd = getRD(instruction);
+                vars.funct3 = getFunct3(instruction);
+                vars.funct7 = getFunct7(instruction);
+                break;
+            case OPCODE_JAL:
+                format = FORMAT_J;
+                vars.rd = getRD(instruction);
+                vars.immediate = getImmediate(instruction);
+                break;
+            default:
+                std::cout << "Unknown opcode: " << std::bitset<7>(opcode) << std::endl;
+                return;
+        }
+
+        std::variant<std::string, Funct7Map> instructionType = Instructions[opcode][vars.funct3];
+
+        if(instructionType.index() == 0)
+        {
+            std::string tempStr = std::get<std::string>(instructionType);
+            printStatement.push_back(tempStr);
+        }
+        else
+        {
+            Funct7Map funct7Map = std::get<Funct7Map>(instructionType);
+            std::string tempStr = funct7Map[vars.funct7];
+            printStatement.push_back(tempStr);
+        }
+
+        addRegisters(vars, printStatement, opcode);
+        printControlSignals(signals);
+
+        // Output the assembled instruction print statement
+        for (size_t i = 0; i < printStatement.size(); ++i) {
+            std::cout << printStatement[i];
+            if (i != printStatement.size() - 1) {
+                std::cout << " ";
+            }
+        }
+        std::cout << std::endl;
+        
+    }
+
+private:
+    // Getters for instruction fields
+    uint32_t getOpcode(uint32_t instruction)
+    {
+        return instruction & 0x7F;
+    }
+
+    uint32_t getRS1(uint32_t instruction)
+    {
+        return (instruction >> 15) & 0x1F;
+    }
+
+    uint32_t getRS2(uint32_t instruction)
+    {
+        return (instruction >> 20) & 0x1F;
+    }
+
+    uint32_t getRD(uint32_t instruction)
+    {
+        return (instruction >> 7) & 0x1F;
+    }
+
+    uint32_t getFunct3(uint32_t instruction)
+    {
+        return (instruction >> 12) & 0x7;
+    }
+
+    uint32_t getFunct7(uint32_t instruction)
+    {
+        return (instruction >> 25) & 0x7F;
+    }
+
+    int32_t getImmediate(uint32_t instruction) {
+        uint32_t opcode = getOpcode(instruction);
+        int32_t imm = 0;
+
+        switch(opcode) {
+            case OPCODE_LOAD:
+            case OPCODE_I_TYPE:
+            case OPCODE_JALR:
+                imm = (instruction >> 20) & 0xFFF;
+                // Sign-extend 12-bit immediate
+                if (imm & 0x800)
+                    imm |= 0xFFFFF000;
+                
+                break;
+            case OPCODE_S_TYPE:
+                imm = ((instruction >> 25) & 0x7F) << 5 |
+                      (instruction >> 7) & 0x1F;
+                // Sign-extend 12-bit immediate
+                if (imm & 0x800)
+                    imm |= 0xFFFFF000;
+                
+                break;
+            case OPCODE_SB_TYPE:
+                imm = ((instruction >> 31) & 0x1) << 12 |
+                      ((instruction >> 7) & 0x1) << 11 |
+                      ((instruction >> 25) & 0x3F) << 5 |
+                      ((instruction >> 8) & 0xF) << 1;
+                // Sign-extend 13-bit immediate
+                if (imm & 0x1000)
+                    imm |= 0xFFFFE000;
+                
+                break;
+            case OPCODE_AUIPC:
+            case OPCODE_LUI:
+                imm = instruction >> 12;
+                // Sign-extend 20-bit immediate
+                if (imm & 0x80000)
+                    imm |= 0xFFF00000;
+                break;
+            case OPCODE_JAL:
+                imm = ((instruction >> 31) & 0x1) << 20 |
+                      ((instruction >> 21) & 0x3FF) << 1 |
+                      ((instruction >> 20) & 0x1) << 11 |
+                      ((instruction >> 12) & 0xFF) << 12;
+                // Sign-extend 21-bit immediate
+                if (imm & 0x100000)
+                    imm |= 0xFFE00000;
+                
+                break;
+            default:
+                imm = 0;
+                break;
+        }
+
+        return imm;
+    }
+
+    void printOperands(int op1, int op2, int op3, std::vector<std::string>& printStatement,
+                    std::vector<bool> isReg, bool isImmediateLast = false) {
+        std::vector<std::string> operands;
+
+        if (op1 != NO_REGISTER) {
+            operands.push_back((isReg[0] ? "x" : "") + std::to_string(op1));
+        }
+
+        if (op2 != NO_REGISTER && op2 != NO_IMMEDIATE) {
+            operands.push_back((isReg[1] ? "x" : "") + std::to_string(op2));
+        }
+
+        if (op3 != NO_REGISTER && op3 != NO_IMMEDIATE) {
+            std::string operand;
+            if (isImmediateLast) {
+                operand = std::to_string(op3);
+            } else {
+                operand = (isReg[2] ? "x" : "") + std::to_string(op3);
+            }
+            operands.push_back(operand);
+        }
+
+        // Add commas between operands
+        for (size_t i = 0; i < operands.size(); ++i) {
+            printStatement.push_back(operands[i]);
+            if (i != operands.size() - 1) {
+                printStatement.back() += ",";
+            }
+        }
+    }
+
+    void addRegisters(InstructionVariables& vars, std::vector<std::string>& printStatement, uint8_t opcode) {
+        switch (opcode) {
+            case OPCODE_R_TYPE:
+                // R-type: rd, rs1, rs2
+                printOperands(vars.rd, vars.rs1, vars.rs2, printStatement, {true, true, true});
+                break;
+            case OPCODE_I_TYPE:
+                // I-type: rd, rs1, imm
+                printOperands(vars.rd, vars.rs1, vars.immediate, printStatement, {true, true, false}, true);
+                break;
+            case OPCODE_LOAD:
+            case OPCODE_LOAD_FP:
+                // Load instructions: rd, offset(rs1)
+                if (vars.rd != NO_REGISTER) {
+                    printStatement.push_back("x" + std::to_string(vars.rd) + ",");
+                }
+                if (vars.immediate != NO_IMMEDIATE && vars.rs1 != NO_REGISTER) {
+                    printStatement.push_back(std::to_string(vars.immediate) + "(x" + std::to_string(vars.rs1) + ")");
+                }
+                break;
+            case OPCODE_S_TYPE:
+            case OPCODE_S_TYPE_FP:
+                // S-type: rs2, offset(rs1)
+                if (vars.rs2 != NO_REGISTER) {
+                    printStatement.push_back("x" + std::to_string(vars.rs2) + ",");
+                }
+                if (vars.immediate != NO_IMMEDIATE && vars.rs1 != NO_REGISTER) {
+                    printStatement.push_back(std::to_string(vars.immediate) + "(x" + std::to_string(vars.rs1) + ")");
+                }
+                break;
+            case OPCODE_SB_TYPE:
+                // B-type: rs1, rs2, imm
+                printOperands(vars.rs1, vars.rs2, vars.immediate, printStatement, {true, true, false}, true);
+                break;
+            case OPCODE_LUI:
+            case OPCODE_AUIPC:
+                // U-type: rd, imm
+                printOperands(vars.rd, vars.immediate, NO_IMMEDIATE, printStatement, {true, false, false}, true);
+                break;
+            case OPCODE_JAL:
+                // J-type: rd, imm
+                printOperands(vars.rd, vars.immediate, NO_IMMEDIATE, printStatement, {true, false, false}, true);
+                break;
+            case OPCODE_JALR:
+                // JALR instruction: rd, offset(rs1)
+                if (vars.rd != NO_REGISTER) {
+                    printStatement.push_back("x" + std::to_string(vars.rd) + ",");
+                }
+                if (vars.immediate != NO_IMMEDIATE && vars.rs1 != NO_REGISTER) {
+                    printStatement.push_back(std::to_string(vars.immediate) + "(x" + std::to_string(vars.rs1) + ")");
+                }
+                break;
+            default:
+                // Handle unknown formats or instructions
+                break;
+        }
+    }
+
+    void printControlSignals(const ControlSignals& signals)
+    {
+        std::cout << "RegWrite = " << signals.RegWrite << "\n"
+                << "MemRead = " << signals.MemRead << "\n"
+                << "MemWrite = " << signals.MemWrite << "\n"
+                << "MemToReg = " << signals.MemToReg << "\n"
+                << "ALUSrc = " << signals.ALUSrc << "\n"
+                << "Branch = " << signals.Branch << "\n"
+                << "Jump = " << signals.Jump << "\n"
+                << "JumpReg = " << signals.JumpReg << "\n"
+                << "Zero = " << signals.Zero << "\n";
+    }
+
+};
+
+//====ASSIGNMENT 2====
 
 const int STALL_INT = 10;       // Stall for integer instructions = 1 CPU cycle = 10 sim ticks
-const int STALL_FLOAT = 50;
+const int STALL_FLOAT = 50;     // Stall for floating point instructions = 5 CPU cycles = 50 sim ticks
 
 struct Event
 {
